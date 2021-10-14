6f9e59f51236e6c74581d3e7991029f3b0473779
Ibrahim H <mohammad.ihakash@gmail.com>
2021-10-14 21:08:32 +0600

    login form desgin fixed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,7 +4,7 @@ import { HiLockClosed } from 'react-icons/hi';
 
 const Login = () => {
     return (
-        <div className="min-h-full flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="flex-grow flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <img
