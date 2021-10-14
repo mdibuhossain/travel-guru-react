e2de2114b5e2ac27ac70397c4097ae475ee96c05
Ibrahim H <mohammad.ihakash@gmail.com>
2021-10-14 20:53:29 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from '../Login/Login';
+import Navbar from './Navbar';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default Header;
