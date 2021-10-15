111f2b5175dbec199ac2e45ccfb8f1e5212f6a06
Ibrahim H <mohammad.ihakash@gmail.com>
2021-10-15 21:26:40 +0600

    default hero added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,39 +1,26 @@
 import React from 'react';
 import './Home.css';
 import { FaArrowCircleRight } from 'react-icons/fa';
-import img1 from '../../Images/Sajek.png';
-import img2 from '../../Images/Sreemongol.png';
-import img3 from '../../Images/sundorbon.png';
+import Hero from '../Hero/Hero';
+import Service from '../../Service/Service';
+import Feature from '../../Feature/Feature';
+import Banner from '../../Banner/Banner';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
-    const images = [
-        img1, img2, img3
-    ]
-
     return (
         <>
-            <div className="relative min-h-screen flex flex-col justify-center">
-                <div className="flex flex-grow banner">
-                    <span className="flex flex-grow opacity-70 bg-gray-900"></span>
-                </div>
-                <div className="absolute z-30 flex justify-center items-center left-0 right-0">
-                    <div className="grid grid-cols-1 lg:grid-cols-2 center">
-                        <div className="text-white">
-                            <div className="w-full lg:w-9/12 mx-auto">
-                                <h1 className="text-5xl md:text-8xl font-bold">COX'S BAZAR</h1>
-                                <p className="py-5 w-90 md:w-9/12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae delectus optio, excepturi harum tempore ipsam illum quos ducimus laborum autem numquam odit nostrum aperiam atque.</p>
-                                <button className="bg-yellow-400 text-gray-900 px-5 py-2 font-semibold rounded-lg">Booking <FaArrowCircleRight className="inline-block" /></button>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-3">
-                            <img className="w-60" src={img1} alt="" />
-                            <img className="w-60" src={img2} alt="" />
-                            <img className="w-60" src={img3} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Hero hero="homeHero">
+                <Banner
+                    title="cox's bazar"
+                    subtitle="deluxe rooms starting at $299"
+                >
+                    <Link className="bg-blue-400 inline-block py-2 px-3 rounded-xl" to='/login'>book now</Link>
+                </Banner>
+            </Hero>
+            {/* <Service></Service>
+            <Feature></Feature> */}
         </>
     );
 };
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -7,7 +7,7 @@ import { useAuth } from '../../Hooks/useAuth';
 const Login = () => {
     const { signWithGoogle } = useAuth();
     return (
-        <div className="flex-grow flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="flex-grow flex items-center justify-center  bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <img
